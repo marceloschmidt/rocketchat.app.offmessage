57b507ea476b3f47a93dd5b62c00111d4b0d9b50
Marcelo Schmidt <marcelo.schmidt@rocket.chat>
2021-09-16 10:19:06 -0300

    Change birthday message

--- enum/App.ts
+++ enum/App.ts
@@ -4,7 +4,7 @@ export enum AppEnum {
     USER_MESSAGED_BOT = 'You have messaged the bot user. This has no effect.',
     MESSAGE_PREVENTED_PTO = 'This person is on a PTO until %s. Would you like to send this message anyway or schedule it for when he/she is back?',
     MESSAGE_PREVENTED_HOLIDAY = 'This person is on a Holiday. Would you like to send this message anyway or schedule it for when he/she is back?',
-    MESSAGE_PREVENTED_BIRTHDAY = 'This person is on a PTO (Birthday). Would you like to send this message anyway or schedule it for when he/she is back?',
+    MESSAGE_PREVENTED_BIRTHDAY = 'Today is this person\'s birthday, so they might be on a PTO. Would you like to send this message anyway or schedule it for tomorrow?',
     MESSAGE_PREVENTED_BUSY_OFFLINE = 'This person is %s. Would you like to send this message anyway?',
     TIMEOUT_MINUTES = '60',
     APP_ENABLED = 'The app is enabled for you.',
